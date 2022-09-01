8c8fa504b778815bce60bfc94573b55cba642221
kawabata <s1931115@s.konan-u.ac.jp>
2022-09-01 09:53:27 +0900

    [Update] 投稿機能の追加

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,15 +1,49 @@
-import React from "react"
-import {auth} from "../firebase/firebase"
+import React, {useState, useEffect} from "react"
+import {db,} from "../firebase/firebase"
 import TweetInput from "./TweetInput";
+import styles from "./Feed.module.css"
+import {doc, onSnapshot, query, collection} from "firebase/firestore";
 
 const Feed: React.FC = () => {
+    const [posts, setPosts] = useState([
+        {
+            id: "",
+            avatar: "",
+            image: "",
+            text: "",
+            timestamp: null,
+            username: ""
+        }
+    ])
+
+    useEffect(() => {
+        // Todo timeStamp 順に並ぶように修正する
+        const q = query(collection(db, "posts"))
+        const unSub = onSnapshot(q, (doc) => {
+            const dataLists = doc.docChanges().map((change) => {
+                const {avatar, image, text, timestamp, username} = change.doc.data()
+                return {
+                    id: change.doc.id,
+                    avatar,
+                    image,
+                    text,
+                    timestamp,
+                    username
+                }
+            });
+            setPosts(dataLists)
+        })
+
+        return () => {
+            unSub()
+        }
+    }, [])
     return (
-        <div>
-            Feed
+        <div className={styles.feed}>
             <TweetInput/>
-            <button onClick={() => auth.signOut()}>
-                Logout
-            </button>
+            {posts.map((post)=> (
+                <h3>{post.id}</h3>
+            ))}
         </div>
     )
 }
